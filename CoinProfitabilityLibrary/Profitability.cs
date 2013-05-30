530bc06a9a1404e197a3879b7b31bd9e643c97fd
Scott Alfter <scott@alfter.us>
2013-05-30 15:19:28 -0700

    more refactoring, and the start of a CLI app

---
+++ CoinProfitabilityLibrary/Profitability.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace ScottAlfter.CoinProfitabilityLibrary
+{
+    public class Profitability
+    {
+        public decimal ProfitOnInterval(long lInterval, decimal dHashRate, decimal dReward, decimal dDifficulty)
+        {
+            BigInteger interval = new BigInteger(lInterval);
+            BigInteger hashrate = new BigInteger(dHashRate);
+            BigInteger reward = new BigInteger(dReward) * 100000000;
+            decimal difficulty = dDifficulty;
+            BigInteger target = ((new BigInteger(65535) << 208) * 100000000000) / new BigInteger(difficulty * 100000000000);
+            BigInteger revenue = interval * target * hashrate * reward / (new BigInteger(1) << 256);
+            return (decimal)revenue / (decimal)100000000;
+        }
+
+        public decimal ProfitOnIntervalBTC(long lInterval, decimal dHashRate, decimal dReward, decimal dDifficulty, decimal dExchangeRate)
+        {
+            return ProfitOnInterval(lInterval, dHashRate, dReward, dDifficulty) * dExchangeRate;
+        }
+    }
+}
---
+++ profit/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScottAlfter.CoinProfitabilityLibrary;
+using Gnu.Getopt;
+
+namespace profit
+{
+    class Program
+    {
+        static void Help()
+        {
+            Console.WriteLine("Usage: profit [options]");
+            Console.WriteLine("");
+            Console.WriteLine("Options:");
+            Console.WriteLine("  -h|--help      this message");
+            Console.WriteLine("  -l|--list      list coins configured in registry");
+            Console.WriteLine("  -c|--coin      select coin to look up");
+            Console.WriteLine("  -b|--bitcoin   expected income in BTC");
+            Console.WriteLine("  -r|--hashrate  override hashrate (provide in H/s)");
+        }
+
+        static void Main(string[] args)
+        {
+            RegistrySettings rs = new RegistrySettings();
+            Getopt go = new Getopt("profit", args, "hlc:br:",
+                new LongOpt[] 
+                { 
+                    new LongOpt("help", Argument.No, null, 'h'),
+                    new LongOpt("list", Argument.No, null, 'l'),
+                    new LongOpt("coin", Argument.Required, null, 'c'),
+                    new LongOpt("bitcoin", Argument.No, null, 'b'),
+                    new LongOpt("hashrate", Argument.Required, null, 'r')
+                });
+            int opt=-1;
+            bool bIncomeInBTC = false;
+            string szSelectedCoin = "Bitcoin";
+
+            while ((opt=go.getopt())!=-1)
+                switch (opt)
+                {
+                    case 'h':
+                        Help();
+                        return;
+                    case 'l':
+                        foreach (string name in rs.GetCoinTypes())
+                            Console.WriteLine(name);
+                        return;
+                    case 'c':
+                        szSelectedCoin = go.Optarg;
+                        break;
+                    case 'b':
+                        bIncomeInBTC = true;
+                        break;
+                    default:
+                        Help();
+                        return;
+                }
+
+        }
+    }
+}
